b528290e0c49181416ac629e81152bff7101407c
Sneha Yadav <snehay136@gmail.com>
2026-09-21 13:18:13 +0530

    g

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const API_BASE = "https://mansik-santulan-score.onrender.com";
+  const API_BASE = "https://mental-health-app-2z4h.onrender.com";
 
   const form = document.getElementById("predict-form");
   const submitBtn = document.getElementById("submit-btn");
